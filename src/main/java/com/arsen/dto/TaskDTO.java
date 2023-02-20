d9cf0b73358aceb8cff2bb85e1f8a9f963f14e12
Arsen <arsproger@gmail.com>
2023-02-20 23:54:35 +0600

    fixed dao bags

--- src/main/java/com/arsen/dto/TaskDTO.java
+++ src/main/java/com/arsen/dto/TaskDTO.java
@@ -5,17 +5,32 @@ import com.arsen.models.User;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
 public class TaskDTO {
+    private final Long id;
+
+    @NotEmpty(message = "Header не может быть пустым!")
+    @Size(max = 20, message = "Превышено максимальное значение для поля header!")
     private String header;
+
+    @Size(max = 50, message = "Превышено максимальное значение для поля description!")
     private String description;
+
+    @NotNull(message = "Deadline не может быть пустым!")
     @Temporal(value = TemporalType.TIMESTAMP)
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private Date deadline;
+
     private TaskStatus taskStatus;
+
     private User owner;
 }
--- src/main/java/com/arsen/dto/UserDTO.java
+++ src/main/java/com/arsen/dto/UserDTO.java
@@ -2,9 +2,22 @@ package com.arsen.dto;
 
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Data
 public class UserDTO {
+    @NotEmpty(message = "Имя не может быть пустым!")
+    @Size(max = 30, message = "Превышено максимальное допустимое значение для имени!")
     private String name;
+
+    @NotEmpty(message = "Email не может быть пустым!")
+    @Email(message = "Некорректный email!")
     private String email;
+
+    @Column(name = "password")
+    @Size(min = 8, message = "Пароль должен содержать больше 8 символов!")
     private String password;
 }
